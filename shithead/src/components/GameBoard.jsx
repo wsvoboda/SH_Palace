14f18abff258a58b1009c449db0a9bdfce8ebab7
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-07-18 00:27:52 -0500

    drag and drop working for card on board

--- shithead/src/components/GameBoard.jsx
+++ shithead/src/components/GameBoard.jsx
@@ -1,45 +1,25 @@
 import React, {useEffect} from 'react'
 import Card from "./Card"
-import CardHolder from './CardHolder'
 import CardPosition from './CardPosition'
-import { moveCard } from './Game'
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
 
-function renderCardHolder(i, cardPosition) {
-  const x = i % 8
-  const y = Math.floor(i / 8)
-  return (
-    <div key={i} style={{ width: '12.5%', height: '12.5%' }}>
-      <CardPosition x={x} y={y}>
-        {renderCard(x, y, cardPosition)}
-      </CardPosition>
-    </div>
-  )
-}
-
-function renderPiece(x, y, [cardX, cardY]) {
+function renderCard(x, y, [cardX, cardY]) {
   if (x === cardX && y === cardY) {
     return <Card />
   }
 }
 
-function handleCardClick(toX, toY) {
-  moveCard(toX, toY)
-}
-
-function renderCard(i, cardPosition) {
+function renderCardHolder(i, cardPosition) {
     const x = i % 8
     const y = Math.floor(i / 8)
-    const isCardHere = x === cardPosition[0] && y === cardPosition[1]
-    const black = (x + y) % 2 === 1
-    const card = isCardHere ? <Card /> : null
-  
     return (
       <DndProvider backend={HTML5Backend}>
-        <div onClick={() => handleCardClick(x, y)} key={i} style={{ width: '12.5%', height: '12.5%' }}>
-          <CardHolder black={black}>{card}</CardHolder>
-        </div>
+      <div key={i} style={{ width: '12.5%', height: '12.5%' }}>
+      <CardPosition x={x} y={y}>
+        {renderCard(x, y, cardPosition)}
+      </CardPosition>
+    </div>
         </DndProvider>
       )
   }
@@ -47,7 +27,7 @@ function renderCard(i, cardPosition) {
 export default function GameBoard({ cardPosition }) {
     const squares = []
     for (let i = 0; i < 64; i++) {
-      squares.push(renderCard(i, cardPosition))
+      squares.push(renderCardHolder(i, cardPosition))
     }
    
     return (

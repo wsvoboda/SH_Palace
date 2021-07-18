4592c6ab8a6f9bd57d97010fd3eed06734aad937
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-07-17 23:54:47 -0500

    update code

--- shithead/src/components/CardPosition.jsx
+++ shithead/src/components/CardPosition.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import CardHolder from './CardHolder'
+import { canMoveKnight, moveCard } from './Game'
+import { ItemTypes } from './Constants'
+import { useDrop } from 'react-dnd'
 
 export default function CardPosition({ x, y, children }) {
     const black = (x + y) % 2 === 1
